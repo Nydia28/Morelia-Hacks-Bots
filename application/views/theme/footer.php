b0d3f5f4c80ae0259409808087197d1fd7e33ee2
joel <softcodec@gmail.com>
2017-07-06 02:28:55 -0500

    Actualizacion Plantilla

---
+++ application/views/theme/footer.php
@@ -0,0 +1,54 @@
+    <!-- Footer Section end -->
+    <footer id="footer" class="root-sec white root-sec footer-wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="clearfix footer-inner">
+              <ul class="left social-icons">
+                <li><a href="#" class="tooltips tooltipped facebook" data-position="top" data-delay="50" data-tooltip="Facebook"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li><a href="#" class="tooltips tooltipped linkedin" data-position="top" data-delay="50" data-tooltip="Linkdin"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li><a href="#" class="tooltips tooltipped twitter" data-position="top" data-delay="50" data-tooltip="Twitter"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li><a href="#" class="tooltips tooltipped google-plus" data-position="top" data-delay="50" data-tooltip="Google Plus"><i class="fa fa-google-plus"></i></a>
+                </li>
+                <li><a href="#" class="tooltips tooltipped dribbble" data-position="top" data-delay="50" data-tooltip="Dribbble"><i class="fa fa-dribbble"></i></a>
+                </li>
+                <li><a href="#" class="tooltips tooltipped behance" data-position="top" data-delay="50" data-tooltip="Behance"><i class="fa fa-behance"></i></a>
+                </li>
+              </ul> <!-- ./social icons end -->
+              <div class="right copyright">
+                <p>MaterialX &copy; All Rights Reserved</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- ./container end-->
+    </footer>
+    <!-- #footer end -->
+
+  </main>
+  <!-- Main Container end-->
+
+
+  <!-- JavaScripts -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.easing.1.3.js"></script>
+  <script src="<?php echo base_url();?>assets/js/detectmobilebrowser.js"></script>
+  <script src="<?php echo base_url();?>assets/js/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/wow.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/waypoints.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.counterup.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.nicescroll.min.js"></script>
+  <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>
+  <script src="<?php echo base_url();?>assets/js/gmaps.js"></script>
+  <script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/materialize.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jwplayer.js"></script>
+  <script src="<?php echo base_url();?>assets/js/sweet-alert.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/common.js"></script>
+  <script src="<?php echo base_url();?>assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ application/views/theme/header.php
@@ -0,0 +1,52 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>MaterialX - Material Design Personal Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="images/favicon.png" >
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/materialize.min.css" media="screen,projection" />
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css" media="screen,projection" />
+
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.min.css" media="screen,projection" />
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/sweet-alert.css" media="screen,projection" />
+
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.carousel.css" media="screen,projection" />
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.transitions.css" media="screen,projection" />
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.theme.css" media="screen,projection" />
+
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
+
+  <!-- Choose your default colors -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/color1.css">
+  <!-- <link rel="stylesheet" href="assets/css/colors/color2.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color3.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color4.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color5.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color6.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color7.css"> -->
+  <!-- <link rel="stylesheet" href="assets/css/colors/color8.css"> -->
+
+  <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
